53bd9a7eb0daecf91c823ac948fde178339f9d45
William Tambe <tambewilliam@gmail.com>
2019-08-20 09:35:08 -0500

    Target PU32 support

---
+++ gcc/config/pu32/pu32-protos.h
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0-only
+// (c) William Fonkou Tambe
+
+extern const char *pu32_output_return ();
+extern void pu32_expand_prologue (void);
+extern void pu32_expand_epilogue (void);
+extern int pu32_initial_elimination_offset (int, int);
+extern rtx gen_pu32_tga (void);
